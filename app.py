e753dfc9aa6eba951f83b38e15f8d2fba7359a7d
andytwoods <andytwoods@gmail.com>
2024-06-28 14:31:19 +0100

    transmitting to unreal working

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import asyncio
+import json
+import os
 
+import requests
 from quart import Quart, render_template, websocket
 
 from broker import Broker
@@ -36,6 +39,48 @@ async def ws() -> None:
     finally:
         task.cancel()
         await task
+
+
+def play(pitch: float, yaw: float, roll: float):
+    # url = 'http://localhost:30010/remote/object/property/'
+    # data = {
+    #     "objectPath": "/Game/Levels/XV3DGS_GaussianFabbrika.XV3DGS_GaussianFabbrika:PersistentLevel.fabbrika_actor_C_UAID_C01803BB37E14AFE01_1235782410.NiagaraComponent",
+    #     "access": "READ_ACCESS",
+    #     "propertyName": "RelativeLocation"
+    # }
+    #
+    # url = 'http://localhost:30010/remote/object/property/'
+    # data = {
+    #     "objectPath": "/Game/Levels/XV3DGS_GaussianFabbrika.XV3DGS_GaussianFabbrika:PersistentLevel.fabbrika_actor_C_UAID_C01803BB37E14AFE01_1235782410.NiagaraComponent",
+    #     "access": "WRITE_ACCESS",
+    #     "propertyName": "RelativeLocation",
+    #     "propertyValue": {"X": 100, "Y": 0, "Z": 0}
+    # }
+
+    url = 'http://localhost:30010/remote/object/call/'
+    data = {
+        "objectPath": "/Game/Levels/XV3DGS_GaussianFabbrika.XV3DGS_GaussianFabbrika:PersistentLevel.fabbrika_actor_C_UAID_C01803BB37E14AFE01_1235782410.RootComponent",
+        "functionName": "SetRelativeRotation",
+        "parameters": {
+            "NewRotation": {
+                "Pitch": pitch,
+                "Yaw": yaw,
+                "Roll": roll
+            }
+        }
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    x = requests.put(url, data=json.dumps(data), headers=headers)
+    print(x, x.text, 22)
+
+
+@app.cli.command("webpack_init")
+def webpack_init():
+    from cookiecutter.main import cookiecutter
+    import webpack_boilerplate
+    pkg_path = os.path.dirname(webpack_boilerplate.__file__)
+    cookiecutter(pkg_path, directory="frontend_template")
 
 
 if __name__ == '__main__':
