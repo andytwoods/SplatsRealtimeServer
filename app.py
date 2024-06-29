61deda66448e811c8991f5fb8404c0e827e197f0
andytwoods <andytwoods@gmail.com>
2024-06-29 21:50:02 +0100

    progeess

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # quart --app app --debug run
 # ./ngrok.exe http http://localhost:5000
+# ngrok http http://127.0.0.1:5000
 
 
 @app.route("/")
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import {FilesetResolver, GestureRecognizer} from "@mediapipe/tasks-vision";
+
 console.log('Hello, Quart with Parcel!');
+// Create task for image file processing:
+
+async function start() {
+    const vision = await FilesetResolver.forVisionTasks(
+        // path/to/wasm/root
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm "
+    );
+    const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task"
+        },
+        numHands: 2
+    });
+}
+
+start();
